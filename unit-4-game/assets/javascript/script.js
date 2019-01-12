50102c9bf1e89fffc8332d4b749372adfa48c71a
Piglet <aurihrp@hotmail.com>
2019-01-12 04:42:52 -0800

    needs styling and goal display on load

---
+++ unit-4-game/assets/javascript/script.js
@@ -0,0 +1,67 @@
+
+var wins = 0;
+var losses = 0;
+var target = 0;
+
+$(document).ready(reset());
+$(document).ready(setGoal());
+
+function setGoal(){
+    target = randomint(19,120);
+    $("#goalNum").text(target);
+}
+
+function crystalShuffle(){
+ 
+    var arr = [];
+  
+    for( i = 0; i <= 3; i++){
+   
+        arr[i] = Math.ceil(Math.random()*12);
+    }
+  
+    crystals = arr;
+    return arr 
+
+}
+
+function randomint(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+
+
+function crystalPoints(valU) {
+
+    $("#scorenum").text(Number($("#scorenum").text()) + crystals[valU]);
+    currentScore = currentScore + crystals[valU];
+    checkResults(currentScore, target);
+
+}
+
+function reset(){
+    setGoal();
+    crystals = crystalShuffle();
+    currentScore = 0;
+    $("#scorenum").text(currentScore);
+}
+
+function checkResults(currentScore, target) {
+    
+    if(currentScore === target){
+        wins++;
+        $("#winsnum").text(wins);
+        crystalShuffle(crystals);
+        target = randomint(19,120);
+        reset();
+    }
+
+    else if(currentScore >= target){
+        losses++;     
+        $("#lossnum").text(losses);
+        crystalShuffle(crystals);
+        target = randomint(19,120);
+        reset();
+    }
+
+}
